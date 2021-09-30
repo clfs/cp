c7cb53d1c7eca096412da9263a896a2cfd52f007
Calvin Figuereo-Supraner <mail@calvin.page>
2021-09-29 20:46:08 -0700

    Solve challenge 2

--- set1.go
+++ set1.go
@@ -3,6 +3,7 @@ package cp
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"fmt"
 )
 
 func HexToBase64(s string) (string, error) {
@@ -11,4 +12,30 @@ func HexToBase64(s string) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+func HexXOR(a, b string) (string, error) {
+	if len(a) != len(b) {
+		return "", fmt.Errorf("different lengths")
+	}
+	ba, err := hex.DecodeString(a)
+	if err != nil {
+		return "", err
+	}
+	bb, err := hex.DecodeString(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(XOR(ba, bb)), nil
+}
+
+func XOR(a, b []byte) []byte {
+	if len(a) != len(b) {
+		panic("different lengths")
+	}
+	out := make([]byte, len(a))
+	for i := range a {
+		out[i] = a[i] ^ b[i]
+	}
+	return out
 }
--- set1_test.go
+++ set1_test.go
@@ -14,3 +14,19 @@ func TestChallenge01(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestChallenge02(t *testing.T) {
+	t.Parallel()
+	var (
+		a    = "1c0111001f010100061a024b53535009181c"
+		b    = "686974207468652062756c6c277320657965"
+		want = "746865206b696420646f6e277420706c6179"
+	)
+	got, err := HexXOR(a, b)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
