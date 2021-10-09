abca85b9fb297ae64d988d29d720c48a096651e0
Calvin Figuereo-Supraner <mail@calvin.page>
2021-10-09 11:55:34 -0700

    Update challenge 8 comment

--- set1_test.go
+++ set1_test.go
@@ -161,7 +161,7 @@ func TestChallenge07(t *testing.T) {
 func TestChallenge08(t *testing.T) {
 	t.Parallel()
 	ct := helperReadHexCiphertexts(t, "testdata/8.txt")
-	// 08649af70dc06f4f and d5d2d69c744cd283 both appear twice at possible offsets
+	// 08649af70dc06f4fd5d2d69c744cd283 is the repeated block
 	want := helperHexDecode(t, "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a")
 	bs := 16
 
